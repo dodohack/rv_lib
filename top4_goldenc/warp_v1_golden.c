a9f1fd53c78bcd25237a1240941e5c2edbfb1ff3
Tristan Qiu <tristan.qiu@dji.com>
2022-03-23 16:21:43 +0800

    warp can autovectorize now

--- top4_goldenc/warp_v1_golden.c
+++ top4_goldenc/warp_v1_golden.c
@@ -9,18 +9,17 @@
 #include <stdlib.h>
 #include "../com_lib/platform.h"
 #include "../com_lib/printf.h"
-
 // #define BOOL int
 // BOOL DEBUG = 0;
 
 struct warp_param{
+    int8_t* restrict src_ptr;
+    int16_t* restrict offset_ptr;
+    int8_t* restrict out_ptr;
     size_t channel;
     size_t height;
     size_t width;
     size_t offset_fl;
-    int8_t* src_ptr;
-    int16_t* offset_ptr;
-    int8_t* out_ptr;
 };
 
 int8_t src_arr_global[571648] = {2};
@@ -30,24 +29,25 @@ int8_t out_arr_global[571648];
 // warp instruction parse
 void warp_param_read(struct warp_param *param, int32_t* rvv_instr_reg) {
     // @TODO implement the real function
+    param->src_ptr = src_arr_global;
+    param->offset_ptr = offset_arr_global;
+    param->out_ptr = out_arr_global;
     param->channel = (size_t)((*(rvv_instr_reg+2) & (int32_t)(0xffff0000)) >> 16);
     param->height = (size_t)(*(rvv_instr_reg+2) & (int32_t)(0x0000ffff));
     param->width = (size_t)((*(rvv_instr_reg+3) & (int32_t)(0xffff0000)) >> 16);
     param->offset_fl = (size_t)(*(rvv_instr_reg+3) & (int32_t)(0x0000ffff));
-    param->src_ptr = src_arr_global;
-    param->offset_ptr = offset_arr_global;
-    param->out_ptr = out_arr_global;
+
 }
 
-void warp_golden(struct warp_param *param){
+void warp_golden(struct warp_param * restrict param){
 
     size_t channel = param->channel;
     size_t height = param->height;
     size_t width = param->width;
     size_t offset_fl = param->offset_fl;
-    int8_t* src_arr = param->src_ptr;
-    int16_t* offset_arr = param->offset_ptr;
-    int8_t* out_arr = param->out_ptr;
+    int8_t* restrict src_arr = param->src_ptr;
+    int16_t* restrict offset_arr = param->offset_ptr;
+    int8_t* restrict out_arr = param->out_ptr;
 
     size_t offset_idx, feat_idx;
     int16_t offset_val, offset_int;
@@ -60,11 +60,17 @@ void warp_golden(struct warp_param *param){
                 offset_idx = h*width + w;
                 offset_val = offset_arr[offset_idx];
                 offset_int = offset_val >> offset_fl;
-                
+                feat_idx = c*height*width + offset_idx;
                 // judge whether idx out of boundary
-                feat_idx = c*height*width + h*width + w;
-                left_val = (((int16_t)(w - offset_int - 1) >= 0) && ((feat_idx - offset_int -1) < channel*height*width)) ? (int16_t)src_arr[feat_idx - offset_int - 1] : 0;
-                right_val = (((int16_t)(w - offset_int) >= 0) && ((feat_idx - offset_int -1) < channel*height*width)) ? (int16_t)src_arr[feat_idx - offset_int] : 0;
+                if ((feat_idx - offset_int -1) < channel*height*width) {
+                    left_val = 0;
+                    right_val = 0;
+                } else {
+                    left_val = (w - offset_int) >= 1 ? (int16_t)src_arr[feat_idx - offset_int - 1] : 0;
+                    right_val = (w - offset_int) >= 0 ? (int16_t)src_arr[feat_idx - offset_int] : 0;
+                }
+                //left_val = (((int16_t)(w - offset_int - 1) >= 0) && ((feat_idx - offset_int -1) < channel*height*width)) ? (int16_t)src_arr[feat_idx - offset_int - 1] : 0;
+                //right_val = (((int16_t)(w - offset_int) >= 0) && ((feat_idx - offset_int -1) < channel*height*width)) ? (int16_t)src_arr[feat_idx - offset_int] : 0;
 
                 // linear interpolation             
                 left_weight = offset_val - (offset_int << offset_fl);
